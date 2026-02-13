a8e6774c37eaaca67c4509d68b7b41f5267ffddb
everton.ramalho <everton.ramalho@sead.pb.gov.br>
2026-02-13 05:07:58 -0300

    refactor: update BankAccount and Product constructors, add Employee class

--- JS/ObJectsAndProperties/objectPropertiesActivities.js
+++ JS/ObJectsAndProperties/objectPropertiesActivities.js
@@ -37,7 +37,7 @@ function createPhone(brand,model,price){
     }
 }
 
-//activity 5
+//activity 5 e 6
 function BankAccount(holder,balance){
     Object.defineProperties(this,{
         holder:{
@@ -54,9 +54,14 @@ function BankAccount(holder,balance){
         },
     })
 
-    this.getBalance = () => {
-        return this.balance
-    }
+    Object.defineProperty(this,'getBalance',{
+        enumerable:false,
+        writable: false,
+        value: ()=>{
+            return this.balance
+        },
+        configurable:false
+    })
 }
 
 
@@ -64,3 +69,78 @@ const nubank = new BankAccount('Nubank','20000')
 console.log(nubank)
 console.log(nubank.getBalance())
 
+
+// Activity 7
+
+const Product = function (name,price,cost,stock) {
+    Object.defineProperties(this,{
+        name:{
+            enumerable:true,
+            value:name,
+            writable:false
+        },
+        price:{
+            enumerable:true,
+            value:price,
+            writable:true
+        },
+        cost:{
+            enumerable:false,
+            value:cost,
+            writable:false
+        },
+        stock:{
+            enumerable:false,
+            value:stock,
+            writable:true
+        }
+    })
+}
+
+const product1 = new Product('Shampoo',20,10,100)
+product1.name = "Condicionador"
+product1.price = 23
+product1.cost = 12
+product1.stock = 50
+console.log(product1)
+console.log(Object.keys(Product))
+
+
+//activity 8 
+
+function Employee(name,role,salary,ssn){
+    Object.defineProperties(this,{
+        name:{
+            enumerable:true,
+            value:name,
+            writable:true,
+            configurable:true
+        },
+        role:{
+            enumerable:true,
+            value:role,
+            writable:true,
+            configurable:true
+        },
+        salary:{
+            enumerable:true,
+            value:salary,
+            writable:false,
+            configurable:true
+        },
+        ssn:{
+            enumerable:false,
+            value:ssn,
+            writable:false,
+            configurable:false
+        }
+        
+    })
+}
+
+const emp1 = new Employee('Alice','Developer',70000,'123-45-6789')
+delete emp1.name
+delete emp1.role
+delete emp1.salary
+delete emp1.ssn
+console.log(Object.keys(emp1))
